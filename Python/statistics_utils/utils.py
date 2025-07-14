64464a53738e5968f5d14b1182bafb8774ad20de
DBlom96 <dannyblom@hotmail.com>
2025-07-14 08:54:55 +0200

    .
    .

--- Python/statistics_utils/utils.py
+++ Python/statistics_utils/utils.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from math import inf, sqrt
-from scipy.stats import norm, t, binom, poisson, chi2, f, ttest_ind, rankdata
+from scipy.stats import norm, t, binom, poisson, chi2, f
+from scipy.stats import chi2_contingency, ttest_ind, rankdata
 from sklearn.linear_model import LinearRegression
 
 # Plotkleuren
